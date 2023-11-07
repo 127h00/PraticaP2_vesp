cdeb642cd34e7e9cb5a7f394cab395c89f983b5c
anna clara <128912768+127h00@users.noreply.github.com>
2023-11-07 19:12:15 -0300

    Add files via upload

---
+++ CHAMA_SP_excluiProduto.sql
@@ -0,0 +1,14 @@
+exec Loja.spExcluiProduto '000010'
+
+select * from loja.produto
+select * from loja.pedido
+
+
+INSERT INTO Loja.produto VALUES
+	('000010', 'Teste', 32, 35.50, 
+    'a nslfhlfhd')
+
+select * from loja.produto
+
+INSERT INTO Loja.pedido VALUES
+    ('64181520005', '000010', 2, 'M')
---
+++ TABELAS_schemaLoja.sql
@@ -0,0 +1,50 @@
+alter schema Loja
+
+create table Loja.funcionario (
+    id_funcionario CHAR(4) NOT NULL PRIMARY KEY,
+    senha VARCHAR(30) NOT NULL
+)
+
+DROP TABLE Loja.funcionario
+SELECT * FROM Loja.funcionario
+
+
+create table Loja.cliente (
+    cpf char(11) NOT NULL PRIMARY KEY,
+    prenome varchar(20) NOT NULL,
+    sobrenome varchar(50) NOT NULL,
+    email varchar(50) UNIQUE NOT NULL,
+    senha varchar(30) NOT NULL,
+    cep char(8) NOT NULL,
+    bairro varchar(60) NOT NULL,
+    rua varchar(60) NOT NULL,
+    numero varchar(3) NOT NULL,
+    complemento varchar(70)
+)
+
+DROP TABLE Loja.cliente
+SELECT * FROM Loja.cliente 
+
+
+create table Loja.produto (
+    id_produto char(6) NOT NULL PRIMARY KEY,
+    nome_produto varchar(60) NOT NULL,
+    estoque INT NOT NULL,
+    preco varchar(8) NOT NULL,
+    descricao varchar(400) NOT NULL
+)
+
+DROP TABLE Loja.produto
+SELECT * FROM Loja.produto
+
+
+create table Loja.pedido (
+    id_pedido INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    cpf_c CHAR(11) NOT NULL FOREIGN KEY REFERENCES Loja.cliente(cpf),
+    produto CHAR(6) NOT NULL FOREIGN KEY REFERENCES Loja.produto(id_produto),
+    quantidade INT NOT NULL,
+    tamanho varchar(2) NOT NULL,
+)
+
+DROP TABLE Loja.pedido
+SELECT * FROM Loja.pedido
