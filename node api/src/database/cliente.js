0c3952c3c66949d383f9e9d0ac5c04b135c729e2
Anna Clara <annaclara2006ferraz@gmail.com>
2023-11-26 21:57:05 -0300

    atualização da api

--- node api/src/database/cliente.js
+++ node api/src/database/cliente.js
@@ -12,7 +12,7 @@ module.exports = {
 
     selecionarPorIdCli: async (cpf) => {
         try {
-          const result = await getConnection().query(`SELECT * FROM loja.cliente WHERE cpf=${cpf}`)
+          const result = await getConnection().query(`SELECT * FROM loja.cliente WHERE cpf='${cpf}'`)
           return result.recordset[0]
         } catch (error) {
           console.log("Error ao selecionar o cliente pelo cpf:"+error)
@@ -21,11 +21,33 @@ module.exports = {
 
     cadastraCliente: async (cpf, prenome, sobrenome, email, senha, cep, bairro, rua, numero, complemento) => {
         try {
-            await getConnection().query(`INSERT INTO loja.cliente VALUES (${cpf}, ${prenome}, ${sobrenome}, ${email}, ${senha},
-                                        ${cep}, ${bairro}, ${rua}, ${numero}, ${complemento})`)
+            await getConnection().query(`INSERT INTO loja.cliente VALUES ('${cpf}', '${prenome}', '${sobrenome}', '${email}', '${senha}',
+                '${cep}', '${bairro}', '${rua}', '${numero}', '${complemento}')`)
             return true
         } catch (err) {
             console.log("Error na criação de Cliente: "+err.code)
+            return false
+        }
+    },
+
+    atualizarCli: async (cpf, cliente) => {
+        try {
+            await getConnection().query(`UPDATE loja.cliente SET prenome = '${prenome}', sobrenome = '${sobrenome}', email = '${email}',
+                senha = '${senha}', cep = '${cep}', bairro = '${bairro}', rua = '${rua}', numero = '${numero}', complemento = '${complemento}'
+                WHERE cpf = '${cpf}'`)
+            return true
+        } catch(err) {
+            console.log("Error ao atualizar Cliente: "+err.code)
+            return false
+        }
+    },
+
+    deletarCli: async (cpf) => {
+        try {
+            await getConnection().query(`DELETE FROM loja.cliente WHERE cpf = '${cpf}'`)
+            return true
+        } catch(err) {
+            console.log("Error ao deletar Cliente: "+err.code)
             return false
         }
     }
