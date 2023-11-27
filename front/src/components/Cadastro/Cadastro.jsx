20179fc8b01934a39ce1e6bcc8ceff46c7bc1587
thiagoccosta <thiagocarvalhoc17@gmail.com>
2023-11-27 17:44:10 -0300

    Começando a adição pagina de cadastro

---
+++ front/src/components/Cadastro/Cadastro.jsx
@@ -0,0 +1,25 @@
+import style from './Cadastro.module.css'
+
+
+function Cadastro(){
+    return <div className={styles.section}>
+        <p>
+            <text></text>
+        </p>
+
+
+
+
+
+
+
+
+
+
+        <p>
+        <button>Submeter</button>
+        </p>
+    </div>
+    
+}
+
