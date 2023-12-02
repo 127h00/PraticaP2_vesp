532cb7af4259ee7b6a488dec83b915bd9749dfd6
AnaluizaJob <analuizajob01@gmail.com>
2023-12-01 23:30:27 -0300

    Adicionando informações a Tela de produto

---
+++ front/src/pages/InfoProdutos/Produtos.jsx
@@ -0,0 +1,13 @@
+import style from "./Produtos.module.css"
+
+function Produtos() {
+    
+    <header>
+        <div className={style.faixa2}>
+            <h2 className={style.h2}>Tempted</h2>
+        </div>
+    </header>
+
+}
+
+export default Produtos
